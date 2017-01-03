87236df9dfcfefbb8680eba1665903dbe8980dfc
Vishnu Govindaraj <vishnu2929@gmail.com>
2017-01-03 04:31:19 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$("a[href$='ContactButton']").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#Contact").offset().top - 150
+  }, 1000);
+  return true;
+});
+$("a[href$='PortfolioButton']").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#Portfolio").offset().top - 150
+  }, 1000);
+  return true;
+});
+$("a[href$='AboutMeButton']").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#AboutMe").offset().top - 150
+  }, 1000);
+  return true;
+});
+
+
+//collapsable menu
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
